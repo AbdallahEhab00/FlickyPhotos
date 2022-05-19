254c5403c8956e7d95777341b2f098c57227ab8d
AbdallahEhab00 <55344010+AbdallahEhab00@users.noreply.github.com>
2022-05-19 15:15:36 +0200

    Add Reusable Generic UITableView

---
+++ FlickyPhotos/UITableView+ext.swift
@@ -0,0 +1,31 @@
+//
+//  UITableView+ext.swift
+//  FlickyPhotos
+//
+//  Created by Abdallah Ehab on 19/05/2022.
+//
+
+import UIKit
+
+extension UITableView{
+    
+    
+    func registerNib<Cell:UITableViewCell>(cell:Cell.Type){
+        
+        let nibName = String(describing: Cell.self)
+        self.register(UINib(nibName: nibName, bundle: nil), forCellReuseIdentifier: nibName)
+    }
+    
+    
+    func dequeue<Cell:UITableViewCell>()->Cell{
+        let identifier = String(describing:Cell.self)
+        
+        guard let cell = dequeueReusableCell(withIdentifier:identifier) as? Cell else {
+            fatalError("Error in Cell")
+        }
+        
+        return cell
+    }
+    
+    
+}
